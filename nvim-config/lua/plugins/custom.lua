26219b980329a4561b4682001f21075f1fb5296b
David Arenas <david.arenas@key.co>
2023-10-24 07:18:22 -0500

    NeoVim/LazyVim & Wezterm config

---
+++ nvim-config/lua/plugins/custom.lua
@@ -0,0 +1,32 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "storm" },
+  },
+  { "rose-pine/neovim", name = "rose-pine", lazy = false, opts = { variant = "moon" } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
+  -- If you'd rather extend the default config, use the code below instead:
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "clojure",
+        "ocaml",
+      })
+    end,
+  },
+}
