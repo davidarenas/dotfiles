3b1d8d60dfb31ea1aa4252e6b34b6cd548f6641a
David Arenas <david.arenas@key.co>
2024-10-24 03:00:40 -0500

    Emulate tmux in wezterm; other small zed changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,32 +7,60 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font(
-  'FiraCode Nerd Font',
-  { weight = 'Medium' }
-)
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
 config.font_size = 15
 -- For example, changing the color scheme:
-config.color_scheme = 'Kanagawa (Gogh)'
+config.color_scheme = "Kanagawa (Gogh)"
 
-config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  {
-	key = 'f',
-	mods = 'LEADER',
-	action = wezterm.action.ToggleFullScreen,
-  },
-  { mods = 'ALT', key = 'Enter', action = wezterm.action.DisableDefaultAssignment },
-  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-  {
-    key = 'a',
-    mods = 'LEADER|CTRL',
-    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
-  },
+	{
+		key = "f",
+		mods = "LEADER",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{ mods = "ALT", key = "Enter", action = wezterm.action.DisableDefaultAssignment },
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "v",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "s",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
 }
 -- and finally, return the configuration to wezterm
 return config
